781aecc339fc827a7df45432edf75cba4e069b3d
matdomis <matheus.domis2000@gmail.com>
2021-06-09 23:04:30 -0300

    Ex 1.2

---
+++ c1/print.c
@@ -0,0 +1,13 @@
+/*
+Exercise 1-2. Experiment to find out what happens when prints 's argument string contains
+			  \c, where c is some character not listed above.
+*/
+
+#include <stdio.h>
+
+void main() {
+  printf("Audible or visual alert. \a\n");  
+  printf("Form feed. \f\n");
+  printf("This escape, \r, moves the active position to the initial position of the current line.\n");
+  printf("Vertical tab \v is tricky, as its behaviour is unspecified under certain conditions.\n");
+}
