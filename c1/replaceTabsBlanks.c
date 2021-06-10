6ed8dade1a61a72eb772ccfe304aa73a98597e60
matdomis <matheus.domis2000@gmail.com>
2021-06-09 23:41:45 -0300

    Ex 1.10

---
+++ c1/replaceTabsBlanks.c
@@ -0,0 +1,28 @@
+/*
+Exercise 1-10. 
+Write a program to copy its input to its output, replacing each tab by \t , each
+backspace by \b , and each backslash by \\ . This makes tabs and backspaces visible in an
+unambiguous way.
+*/
+
+#include <stdio.h>
+
+void main() {
+	char c;
+
+	for(; (c = getchar()) != EOF ;)
+		if (c == '\t') {
+			putchar('\\');
+			putchar('t');
+		}
+		else if (c == '\\') {
+			putchar('\\');
+			putchar('\\');
+		}
+		else if (c == '\b') {
+			putchar('\\');
+			putchar('b');
+		}
+		else
+			putchar(c);
+}
