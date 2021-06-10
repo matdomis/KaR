d4ffe0ba24edc2de206e1b399e9d62cd579152cb
matdomis <matheus.domis2000@gmail.com>
2021-06-09 23:13:46 -0300

    Ex 1.3

---
+++ c1/headingFahrenheit.c
@@ -0,0 +1,22 @@
+/*
+Exercise 1-3. Modify the temperature conversion program to print a heading above the table.
+*/
+
+#include <stdio.h>
+
+void main() {
+	float fahr, celsius;
+	float lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	
+	printf(" °C\t°F\n");
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
